e77eda37d3859a1a2d888fc4b140540ab1cf3cb7
Lili <iliyanastefanova@msn.com>
2019-03-17 20:36:48 -0400

    files

---
+++ app/data/friends.js
@@ -0,0 +1,101 @@
+// an array that holds all of the friends data
+var friendsData = [
+	{
+		"name": "Lola",
+		"photo": "https://66.media.tumblr.com/c5e0f53e9e4b1bc6da57e727f737753b/tumblr_piet9qDkNN1tjl5sho1_400.jpg",
+		"scores": [
+			3,
+			4,
+			1,
+			5,
+			3,
+			5,
+			2,
+			4,
+			1,
+			2
+		]
+	},
+	{
+		"name": "Cinna",
+		"photo": "https://66.media.tumblr.com/797a767b65adee09ed13f067f4abf503/tumblr_opjp8lCapZ1si0kwmo1_400.jpg",
+		"scores": [
+			2,
+			3,
+			4,
+			4,
+			2,
+			3,
+			4,
+			5,
+			3,
+			3
+		]
+	},
+	{
+		"name": "Beki",
+		"photo": "https://66.media.tumblr.com/403a0e9c70ad0ff1d4bf239333e51b3f/tumblr_owlmzzYsdB1sii6a0o1_400.png",
+		"scores": [
+			1,
+			4,
+			3,
+			4,
+			3,
+			4,
+			2,
+			3,
+			4,
+			4
+		]
+	},
+	{
+		"name": "Storm",
+		"photo": "https://66.media.tumblr.com/4cb36d6ccdbc2da9ab2b750e1d6fb0ce/tumblr_ovcrpt09rf1vy2tgqo2_400.png",
+		"scores": [
+			1,
+			2,
+			1,
+			2,
+			1,
+			2,
+			2,
+			1,
+			2,
+			1
+		]
+	},
+	{
+		"name": "Pup",
+		"photo": "https://myvideogamelist.com/images/userpics/b43e4fab30tumblr_static_ezwi4bg8h2os04wg4og404k0w.jpg",
+		"scores": [
+			4,
+			5,
+			5,
+			4,
+			5,
+			4,
+			5,
+			4,
+			5,
+			5
+		]
+	},
+	{
+		"name": "Sunny",
+		"photo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTSn9ZfZJfxC_EFHkYS_xX_siKJExdP8J-p4ntQ1dZnrobrAAuTWw",
+		"scores": [
+			4,
+			5,
+			5,
+			3,
+			5,
+			4,
+			5,
+			2,
+			1,
+			5
+		]
+	}
+];
+
+module.exports = friendsData;
